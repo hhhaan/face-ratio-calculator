9c393d97a39fbd5a26345778a39d462d7efd1d21
hhhaan <hhanheon@gmail.com>
2025-02-28 12:27:29 +0900

    testpage추가

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,12 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { MainPage } from '@/pages/MainPage';
+import { MainPage, TestPage } from '@/pages/MainPage';
 
 function App() {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<MainPage />} />
+                <Route path="/test" element={<TestPage />} />
             </Routes>
         </BrowserRouter>
     );
